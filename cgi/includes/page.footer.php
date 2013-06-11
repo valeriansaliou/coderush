149eb5670addffb3b8b70abf11d491050de42352
Valérian Saliou <val.saliou@gmail.com>
2013-06-11 16:11:41 +0200

    Lang picker performance fix

--- cgi/includes/page.footer.php
+++ cgi/includes/page.footer.php
@@ -11,8 +11,8 @@
 	<!-- PAGE FOOTER HERE -->
 
 	<ul class="lang">
-		<?php foreach(listTranslation() as $lang_index => $lang_arr): ?>
 		<?php $string_request = stringRequest(false); ?>
+		<?php foreach(listTranslation() as $lang_index => $lang_arr): ?>
 			<?php if($lang_index > 0) echo ' - '; ?>
 			<li>
 				<a<?php if($CONTEXT_LANG == $lang_arr['code']) echo ' class="current"'; ?> href="/<?php echo $lang_arr['code'].$string_request; ?>" data-lang="<?php echo $lang_arr['code']; ?>"><?php echo htmlspecialchars($lang_arr['name']); ?></a>
